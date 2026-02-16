0be17269dc5b6b1e494a127ac40b518b03495b55
Kami <197647764+Kami516@users.noreply.github.com>
2026-02-16 23:17:04 +0100

    Time: 304 ms (27.82%), Space: 67.5 MB (90.83%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    actor_director = actor_director.groupby(['actor_id','director_id'])['timestamp'].count().reset_index()
+    actor_director = actor_director[actor_director['timestamp']>=3]
+    del actor_director['timestamp']
+
+    return actor_director
